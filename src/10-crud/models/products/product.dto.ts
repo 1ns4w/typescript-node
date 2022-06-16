227d819eedfd2a91e4ce3181aaccc02353686f8b
Angel Mejia <amejiadev@gmail.com>
2022-06-16 18:23:42 -0500

    refactor: product data

--- src/10-crud/models/products/product.dto.ts
+++ src/10-crud/models/products/product.dto.ts
@@ -1,6 +1,6 @@
 import { ProductInterface } from "./product.model";
 
-export interface ProductDTO extends Omit<ProductInterface, 'id' | 'createdAt' | 'updatedAt' | 'category' > {
+export interface ProductDTO extends Omit<ProductInterface, 'id' | 'createdAt' | 'updatedAt' | 'category'> {
   categoryId: string;
 }
 
--- src/10-crud/models/products/product.service.ts
+++ src/10-crud/models/products/product.service.ts
@@ -1,9 +1,21 @@
 import { ProductInterface } from "./product.model";
 import { ProductDTO } from "./product.dto";
+import { faker } from "@faker-js/faker";
 
 export let products: ProductDTO[] = []
 
-export const addProduct = (product: ProductDTO): void => { products.push(product) }
+// product creations is delegated to service, that's what we use dtos for
+export const addProduct = (product: ProductDTO): void => {
+  products.push({
+    id: faker.datatype.uuid(),
+    createdAt: faker.date.recent(),
+    category: {
+      id: faker.datatype.uuid(),
+      name: faker.commerce.department()
+    },
+    ...product,
+  })
+}
 
 /*
 export const updateProduct = (productId: string, product: ProductDTO): void => {
