3c057403e76b1e1fb454d1faf16b6fcfa4e48bcd
Angel Mejia <amejiadev@gmail.com>
2022-06-16 18:06:10 -0500

    docs: add notes for interfaces

--- src/10-crud/models/base.model.ts
+++ src/10-crud/models/base.model.ts
@@ -1,5 +1,7 @@
 export type IdAlias = number | string;
 
+// dtos are used to define mapped data between models and use it for extending models
+// you can extend specific data using pick or omit
 export interface BaseInterface {
   // readonly prevents values from being changed after creation
   readonly id: IdAlias;
